344efb0fae49e92f608298522db9e7fdba99f524
Nguyen Huy Hoang <huyhoang211226@gmail.com>
2026-01-25 00:04:59 +0700

    haha

---
+++ video17/video17.js
@@ -0,0 +1,16 @@
+console.log("Hello word with javascrip day la video 17")
+
+var name = 'Nguyễn Huy Hoàng';
+
+const name1 ="Hoàng dep zai";
+
+let name3 ="Hoàng tán gái"
+
+console.log(name);
+
+let age = 13;
+age=12;
+console.log(age);
+
+const MyAge = 18;
+console. log(MyAge)
---
+++ video18/video18.js
@@ -0,0 +1,20 @@
+console.log("video 18")
+//number
+
+const myAge = 19;
+const myScore =9.8;
+console.log(myAge);
+console.log(myScore);
+
+
+//String
+
+const name = "Hoang dep zai";//java
+
+const name1 ='Tieu cho';
+
+const name3 = `Dinh cao`;
+
+console.log(name);
+
+
